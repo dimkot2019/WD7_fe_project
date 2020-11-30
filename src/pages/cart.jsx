f0a2ef770c334ee4ad806f599099fcf9dc56b9b7
dimkot2019 <58875821+dimkot2019@users.noreply.github.com>
2020-11-30 09:48:32 +0300

    version 3

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ProductsOfCart from '../components/catalogelement/productsOfCart';
 
+function SubTotal(key) {
+    let data = JSON.parse(localStorage.getItem(key)) || [];
+    if (data.length === 0) {
+        return 0;
+    }
+    if (data.length === 1) {
+        return data[0].res5;
+    }
+    if (data.length > 1) {
+        return data.reduce(function(prev,current) {return prev.res5 + current.res5;});
+    }
+}
 
-function CartPage () { 
+
+function CartPage () {
 
     let data = JSON.parse(localStorage.getItem('Cart')) || [];
 
-    const subTotal = data.reduce(function(prev,current) {return prev.res5 + current.res5;});
+    let subTotal = SubTotal('Cart');
+
     
     return (
                 <div className="cart-table-area section-padding-100">
@@ -46,9 +60,9 @@ function CartPage () {
                                 <div className="cart-summary">
                                     <h5>Cart Total</h5>
                                     <ul className="summary-table">
-                                        <li><span>subtotal:</span> <span>${subTotal}</span></li>
+                                        <li><span>subtotal:</span> <span>${`${subTotal}`}</span></li>
                                         <li><span>delivery:</span> <span>Free</span></li>
-                                        <li><span>total:</span> <span>${subTotal}</span></li>
+                                        <li><span>total:</span> <span>${`${subTotal}`}</span></li>
                                     </ul>
                                     <div className="cart-btn mt-100">
                                         <a href="cart.html" className="btn amado-btn w-100">Checkout</a>
